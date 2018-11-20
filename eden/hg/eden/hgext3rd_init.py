df874846d9c9a6d863f157dc0765ad3c3cd2abc4
Adam Simpkins <simpkins@fb.com>
2018-11-19 18:22:41 -0800

    fix the remaining type errors in eden python code
    Summary: Fix the remaining set of errors reported by pyre and mypy.
    
    Reviewed By: strager
    
    Differential Revision: D13086855
    
    fbshipit-source-id: 4c2b21352f94ef225a5555aef0f6b95b92e56f6d

--- eden/hg/eden/hgext3rd_init.py
+++ eden/hg/eden/hgext3rd_init.py
@@ -4,13 +4,11 @@
 # This software may be used and distributed according to the terms of the
 # GNU General Public License version 2.
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
+from __future__ import absolute_import, division, print_function, unicode_literals
 
 import pkgutil
 
+
 # Indicate that hgext3rd is a namspace package, and other python path
 # directories may still be searched for hgext3rd extensions.
-__path__ = pkgutil.extend_path(__path__, __name__)
+__path__ = pkgutil.extend_path(__path__, __name__)  # type: ignore # noqa: F821
